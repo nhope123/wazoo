d2c305dad38c54da85a74ddc5533687981309c18
Nial Hope <nyevincy@gmail.com>
2024-11-20 23:38:43 -0500

    Enhance Header component: add social media icons for GitHub and LinkedIn, and refactor imports for better organization

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,18 @@
+import CloseRounded from '@mui/icons-material/CloseRounded';
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import MenuRounded from '@mui/icons-material/MenuRounded';
+import NightlightRounded from '@mui/icons-material/NightlightRounded';
+import { Box } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import { SxProps, Theme } from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
 import { FC } from 'react';
-import IconButton from '@mui/material/IconButton';
-import MenuRounded from '@mui/icons-material/MenuRounded';
-import { HeaderProps } from './types';
-import CloseRounded from '@mui/icons-material/CloseRounded';
-import { Box } from '@mui/material';
-import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
-import NightlightRounded from '@mui/icons-material/NightlightRounded';
 import Branding from './components/Branding/Branding';
+import { HeaderProps } from './types';
+import GitHub from '@mui/icons-material/GitHub';
+import LinkedIn from '@mui/icons-material/LinkedIn';
 
 const rootSx: SxProps<Theme> = {
   backgroundColor: 'background.paper',
@@ -23,6 +25,16 @@ const iconSx: SxProps<Theme> = {
   display: { xs: 'inline-block', md: 'none' },
   marginRight: 2,
 };
+
+const SOCIALS = [
+  { title: 'GitHub', url: 'https://github.com/nhope123/wazoo', icon: <GitHub /> },
+  {
+    title: 'Linkedin',
+    url: ' https://www.linkedin.com/in/nialhope/',
+    icon: <LinkedIn />,
+    color: 'info',
+  },
+];
 
 const Header: FC<HeaderProps> = (props) => {
   const { isDrawerStateOpen, setDrawerOpenState, mode, setMode } = props;
@@ -56,6 +68,17 @@ const Header: FC<HeaderProps> = (props) => {
                 <NightlightRounded sx={{ color: '#0cc6d2' }} />
               )}
             </IconButton>
+            {SOCIALS.map((social) => (
+              <IconButton
+                color={social?.color as IconButtonProps['color']}
+                key={social.title}
+                href={social.url}
+                target="_blank"
+                title={`Visit ${social.title}`}
+              >
+                {social.icon}
+              </IconButton>
+            ))}
           </Box>
         </Toolbar>
       </Container>
